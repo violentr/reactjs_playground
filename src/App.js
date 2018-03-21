9c453f38cc42129d6b85825357bf610e2846628d
Deniss <den.b@itservicesgroup.co.uk>
2018-03-21 23:02:19 +0000

    Add Initial state with array of persons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    persons: [
+      {name: 'Max', age: 21},
+      {name: "Stephany", age: 21}
+    ]
+  }
+
   render() {
+    const persons = this.state.persons
     return (
       <div className="App">
         <h1> React App </h1>
-        <Person name="Max" age= "28"> My Hobbie is Raicing </Person>
-        <Person name="Stephany" age= "21"/>
+        <Person name={persons[0].name} age={persons[0].age}> My Hobbie is Raicing </Person>
+        <Person name={persons[1].name} age={persons[1].age}/>
       </div>
     );
   }
