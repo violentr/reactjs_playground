57052775eba75983dca285ba066b239eb2513fcd
Deniss <den.b@itservicesgroup.co.uk>
2018-03-21 23:16:40 +0000

    Add Change state when button clicked

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ class App extends Component {
       {name: "Stephany", age: 21}
     ]
   }
-
+  switchName = (newName) =>{
+    this.setState({persons: [
+      {name: newName, age: 21},
+      {name: "Stephany", age: 21}
+    ]
+   })
+  }
   render() {
     const persons = this.state.persons
     return (
       <div className="App">
         <h1> React App </h1>
+        <button onClick={() => this.switchName('Maximilian')}>Swith Name </button>
         <Person name={persons[0].name} age={persons[0].age}> My Hobbie is Raicing </Person>
-        <Person name={persons[1].name} age={persons[1].age}/>
+        <Person name={persons[1].name} age={persons[1].age}
+         click={this.switchName.bind(this, "Max!")}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return(
     <div>
-      <p> I'am a person {props.name}, I am {props.age} years old</p>
+      <p onClick={props.click}> I'am a person {props.name}, I am {props.age} years old</p>
       <div> {props.children} </div>
     </div>
   )
