755f85f1634cd7f5a8f4501eac6a718f2d7edf71
Deniss <den.b@itservicesgroup.co.uk>
2018-03-21 22:39:16 +0000

    Add Simple Person component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1> React App </h1>
+        <Person />
       </div>
     );
   }
