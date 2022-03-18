5634d8ac58e11d37c35540598e197a2579c7aeea
Deniss <den.b@itservicesgroup.co.uk>
2022-03-18 15:43:29 +0000

    Add Services page

---
+++ src/components/Pages/Services.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Header from '../Common/Header' ;
+
+class Services extends Component {
+    render(){
+        return(
+            <div>
+                <Header 
+                    title="Welcome To Our Studio!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="tell me more"
+                    link="/services"
+                    showButton={true}    
+                />
+                <section className="page-section" id="services">
+                    <div className="container">
+                        <div className="text-center">
+                            <h2 className="section-heading text-uppercase">Services</h2>
+                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                        </div>
+                        <div className="row text-center">
+                            <div className="col-md-4">
+                                <span className="fa-stack fa-4x">
+                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                                    <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <h4 className="my-3">E-Commerce</h4>
+                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                            </div>
+                            <div className="col-md-4">
+                                <span className="fa-stack fa-4x">
+                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                                    <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <h4 className="my-3">Responsive Design</h4>
+                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                            </div>
+                            <div className="col-md-4">
+                                <span className="fa-stack fa-4x">
+                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                                    <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <h4 className="my-3">Web Security</h4>
+                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+export default Services;
