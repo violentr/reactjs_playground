6906002664146945e0fc05578ded9d639df8809a
Deniss <den.b@itservicesgroup.co.uk>
2022-03-18 15:51:32 +0000

    Add Route /portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Services from './components/Pages/Services';
+import Portfolio from './components/Pages/Portfolio';
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import './App.css';
 
@@ -20,6 +21,9 @@ function App() {
           />
           <Route path="/services"
             element={<Services />}
+          />
+          <Route path="/portfolio"
+            element={<Portfolio />}
           />
         </Routes>
       </PageWrapper>
