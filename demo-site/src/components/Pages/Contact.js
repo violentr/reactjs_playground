46afb47e6f35a54545ee783b068c7b64ed59902a
Deniss <den.b@itservicesgroup.co.uk>
2022-03-18 17:24:59 +0000

    Add correct class attribute

--- demo-site/src/components/Pages/Contact.js
+++ demo-site/src/components/Pages/Contact.js
@@ -12,47 +12,47 @@ class Contact extends Component {
                     link="/services"
                     showButton={true}    
                 />
-                <section class="page-section" id="contact">
-                    <div class="container">
-                        <div class="text-center">
-                            <h2 class="section-heading text-uppercase">Contact Us</h2>
-                            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <section className="page-section" id="contact">
+                    <div className="container">
+                        <div className="text-center">
+                            <h2 className="section-heading text-uppercase">Contact Us</h2>
+                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                         </div>
                         <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                            <div class="row align-items-stretch mb-5">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
-                                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            <div className="row align-items-stretch mb-5">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input className="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
+                                        <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
                                     </div>
-                                    <div class="form-group">
+                                    <div className="form-group">
                                   
-                                        <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
-                                        <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                        <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                                        <input className="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                        <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                        <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
                                     </div>
-                                    <div class="form-group mb-md-0">  
-                                        <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
-                                        <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                                    <div className="form-group mb-md-0">  
+                                        <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
+                                        <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group form-group-textarea mb-md-0">  
-                                        <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
-                                        <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                <div className="col-md-6">
+                                    <div className="form-group form-group-textarea mb-md-0">  
+                                        <textarea className="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                                        <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center text-white mb-3">
-                                    <div class="fw-bolder">Form submission successful!</div>
+                            <div className="d-none" id="submitSuccessMessage">
+                                <div className="text-center text-white mb-3">
+                                    <div className="fw-bolder">Form submission successful!</div>
                                     To activate this form, sign up at
                                     <br />
                                     <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                                 </div>
                             </div>  
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
-                            <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
+                            <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
+                            <div className="text-center"><button className="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
                         </form>
                     </div>
                 </section>
