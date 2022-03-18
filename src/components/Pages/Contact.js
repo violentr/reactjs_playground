dced4af8b2c2dae50be8f46fb28597acabf6aa88
Deniss <den.b@itservicesgroup.co.uk>
2022-03-18 16:07:03 +0000

    Add Contact page

---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import Header from '../Common/Header';
+
+class Contact extends Component {
+    render(){
+        return(
+            <div>
+                 <Header 
+                    title="Welcome To Our Studio!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="tell me more"
+                    link="/services"
+                    showButton={true}    
+                />
+                <section class="page-section" id="contact">
+                    <div class="container">
+                        <div class="text-center">
+                            <h2 class="section-heading text-uppercase">Contact Us</h2>
+                            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                        </div>
+                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                            <div class="row align-items-stretch mb-5">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
+                                        <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                    </div>
+                                    <div class="form-group">
+                                  
+                                        <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                        <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                        <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                                    </div>
+                                    <div class="form-group mb-md-0">  
+                                        <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
+                                        <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-textarea mb-md-0">  
+                                        <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                                        <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center text-white mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>  
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                            <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
+                        </form>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+    
+}
+export default Contact;
