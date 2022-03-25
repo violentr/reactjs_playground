728e3e51966215426a3cf459c44ed55d93b3604f
Deniss <den.b@itservicesgroup.co.uk>
2022-03-25 12:34:16 +0000

    Add Style Services with SingleService component

--- demo-site/src/components/Pages/Services.js
+++ demo-site/src/components/Pages/Services.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../Common/Header' ;
+import SingleService from '../Common/SingleService';
+
+const services = [
+    {title: 'E-Commerce', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', icon: 'fa-shopping-cart'},
+    {title: 'Responsive Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', icon: 'fa-laptop'},
+    {title: 'Web Security', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.', icon: 'fa-lock'}
+
+
+]
 
 class Services extends Component {
     render(){
@@ -19,30 +28,9 @@ class Services extends Component {
                             <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                         </div>
                         <div className="row text-center">
-                            <div className="col-md-4">
-                                <span className="fa-stack fa-4x">
-                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                                    <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                                </span>
-                                <h4 className="my-3">E-Commerce</h4>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                            </div>
-                            <div className="col-md-4">
-                                <span className="fa-stack fa-4x">
-                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                                    <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
-                                </span>
-                                <h4 className="my-3">Responsive Design</h4>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                            </div>
-                            <div className="col-md-4">
-                                <span className="fa-stack fa-4x">
-                                    <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                                    <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
-                                </span>
-                                <h4 className="my-3">Web Security</h4>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                            </div>
+                            {services.map((service, index) => {
+                                return <SingleService {...service} key={index}/>
+                            })}
                         </div>
                     </div>
                 </section>
