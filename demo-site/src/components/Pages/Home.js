7231219db07c413b5cc459d4c3b1ede4e0181e42
Deniss <den.b@itservicesgroup.co.uk>
2022-03-25 14:00:36 +0000

    Add Services and Portfolio component to Home page

--- demo-site/src/components/Pages/Home.js
+++ demo-site/src/components/Pages/Home.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import Header from '../Common/Header';
+import Services from './Services';
+import Portfolio from './Portfolio';
 
 class Home extends Component {
     render(){
         return(
-        <Header 
-            title="Welcome To Our Studio!"
-            subtitle="IT'S NICE TO MEET YOU"
-            buttonText="tell me more"
-            link="/services"
-            showButton={true}    
-        />
+        <div>
+            <Header
+                title="Welcome To Our Studio!"
+                subtitle="IT'S NICE TO MEET YOU"
+                buttonText="tell me more"
+                link="/services"
+                showButton={true}
+            />
+            <Services />
+            <Portfolio />
+        </div>
         )
     }
 }
