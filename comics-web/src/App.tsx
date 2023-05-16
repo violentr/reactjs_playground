7c8cae9731947cfa43d871b32dfa11e05789809c
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2023-05-16 19:24:27 +0100

    Add props to ListGroup component

--- comics-web/src/App.tsx
+++ comics-web/src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css'
-import ListGroup from './components/ListGroup'
+import "./App.css";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-
+  const cities = ["New York", "London", "Tokyo"];
   return (
     <>
       <div>
-        <ListGroup />
+        <ListGroup items={cities} heading="Cities" />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- comics-web/src/components/ListGroup.tsx
+++ comics-web/src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["New York", "London", "Tokyo"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup(props: Props) {
+  const { items, heading } = props;
+  // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.map((item, index) => (
           <li
