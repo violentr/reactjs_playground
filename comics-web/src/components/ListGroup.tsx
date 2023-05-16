b42faafef3e53b3bdbbc7346ce100941bbcae57f
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2023-05-16 19:12:55 +0100

    Add active item from the list highlighted

--- comics-web/src/components/ListGroup.tsx
+++ comics-web/src/components/ListGroup.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["New York", "London", "Tokyo"];
-
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
